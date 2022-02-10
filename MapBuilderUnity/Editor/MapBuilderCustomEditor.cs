64aa14611f7cedccc4384b3dff5ee6b372e64c27
Adrián <58742147+dip000@users.noreply.github.com>
2022-02-09 19:33:42 -0600

    Added buttons

---
+++ MapBuilderUnity/Editor/MapBuilderCustomEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapBuilderManager))]
+public class MapBuilderCustomEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		//EditorGUILayout.LabelField("Select Graphics for each item. Otherwise, will use placeholders");
+		base.OnInspectorGUI();
+		EditorGUILayout.Space(20);
+
+
+		MapBuilderManager builder = (MapBuilderManager)target;
+
+
+		/*EditorGUILayout.LabelField("To automatically create a holder for your graphics");
+		EditorGUILayout.LabelField("so they are placed correctly");*/
+		if (GUILayout.Button("Update Map"))
+			builder.MakeLevel();
+
+		if (GUILayout.Button("Reset Map"))
+			builder.ResetItems();
+
+	}
+}
